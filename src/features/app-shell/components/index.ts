b1312882b24465365b4ef50e21a97aed3640c77d
Jonas <34908654+Jon2050@users.noreply.github.com>
2026-07-22 09:06:30 +0200

    Remove unused project files
    Agent: Codex

--- src/features/app-shell/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as BottomSheet } from './BottomSheet.svelte';
-export { default as DeploymentInfoFooter } from './DeploymentInfoFooter.svelte';
-export { default as ErrorBoundaryPlaceholder } from './ErrorBoundaryPlaceholder.svelte';
-export { default as LoadingPlaceholder } from './LoadingPlaceholder.svelte';
-export { default as SkeletonCard } from './SkeletonCard.svelte';
-export { default as ToastContainer } from './ToastContainer.svelte';
-export { default as ServiceWorkerUpdateBanner } from './ServiceWorkerUpdateBanner.svelte';
